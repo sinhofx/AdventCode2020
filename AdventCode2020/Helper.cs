c85e1347838c10f7067485b2aec972d188362d8a
Doug Day <dougday273@gmail.com>
2020-12-10 14:18:27 -0600

    Day 9! This one was simple.

--- AdventCode2020/Helper.cs
+++ AdventCode2020/Helper.cs
@@ -42,9 +42,13 @@ namespace AdventCode2020
             //Console.WriteLine(Problem7.part1(input));
             //Console.WriteLine(Problem7.part2(input));
 
-            input = Helper.Input("D:\\Code\\cs\\AdventCode2020\\AdventCode2020\\input\\problem8.txt");
-            Console.WriteLine(Problem8.part1(input));
-            Console.WriteLine(Problem8.part2(input));
+            //input = Helper.Input("D:\\Code\\cs\\AdventCode2020\\AdventCode2020\\input\\problem8.txt");
+            //Console.WriteLine(Problem8.part1(input));
+            //Console.WriteLine(Problem8.part2(input));
+
+            input = Helper.Input("D:\\Code\\cs\\AdventCode2020\\AdventCode2020\\input\\problem9.txt");
+            Console.WriteLine(Problem9.part1(input));
+            Console.WriteLine(Problem9.part2(input));
 
             Console.Read();
 
---
+++ AdventCode2020/Problem9.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventCode2020
+{
+    class Problem9
+    {
+        public static long part1(List<string> input)
+        {
+            bool match = false;
+            long[] arrInput = new long[input.Count];
+            for (int i = 0; i < input.Count; i++)
+            {
+                arrInput[i] = Int64.Parse(input.ToArray()[i]);
+            }
+
+            for (int i = 25; i < arrInput.Length; i++)
+            {
+                match = false;
+                for (int j = i - 25; j < i; j++)
+                {
+                    for (int k = i - 25; k < i; k++)
+                    {
+                        if ((arrInput[j] + arrInput[k] == arrInput[i]) && j != k)
+                        {
+                            match = true;
+                        }
+                    }
+                }
+                if (!match)
+                {
+                    return arrInput[i];
+                }
+            }
+            return 0;
+        }
+
+        public static long part2(List<string> input)
+        {
+            long[] arrInput = new long[input.Count];
+            for (int i = 0; i < input.Count; i++)
+            {
+                arrInput[i] = Int64.Parse(input.ToArray()[i]);
+            }
+
+            long currentIndex = 0;
+            long sum = 0;
+            long goal = 25918798;
+            List<long> numbers = new List<long>();
+
+            for (long i = 0; i < arrInput.Length;)
+            {
+                sum += arrInput[i];
+                numbers.Add(arrInput[i]);
+                if (sum == goal)
+                {
+                    break;
+                }
+                if (i + 1 == arrInput.Length)
+                {
+                    numbers.Clear();
+                    currentIndex++;
+                    i = currentIndex;
+                    sum = 0;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            long low = numbers.ToArray()[0];
+            long high = numbers.ToArray()[0];
+            foreach (long l in numbers)
+            {
+                if (l < low)
+                {
+                    low = l;
+                }
+                if (l > high)
+                {
+                    high = l;
+                }
+            }
+            return low + high;
+
+        }
+    }
+}
